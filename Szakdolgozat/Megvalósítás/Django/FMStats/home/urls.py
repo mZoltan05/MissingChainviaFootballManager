5d259aa2a3b34e434de174ecca87f65308482f3d
Munkácsi Zoltán <munkacsizoltan05@gmail.com>
2022-04-20 22:41:18 +0200

    club done

--- Szakdolgozat/Megvalósítás/Django/FMStats/home/urls.py
+++ Szakdolgozat/Megvalósítás/Django/FMStats/home/urls.py
@@ -6,5 +6,8 @@ from . import views
 urlpatterns = [
    # path('',views.home, name='home'),
     #path('add',views.add, name='index')
-    path('',views.home, name='home')
+    path('',views.home, name='home'),
+    path(r'^clubwithchosen/(?P<chosenclub>)/$',views.clubwithchosen, name='clubwithchosen'),
+    path('club',views.club, name='club')
+    
 ]
--- Szakdolgozat/Megvalósítás/Django/FMStats/home/views.py
+++ Szakdolgozat/Megvalósítás/Django/FMStats/home/views.py
@@ -1,3 +1,5 @@
+from http.client import SWITCHING_PROTOCOLS
+from pickle import FALSE
 from traceback import format_exc
 from urllib import request
 from django.forms import formset_factory
@@ -34,17 +36,37 @@ def home(request):
     int_searches_bottom = {'age_bottom':'','height_bottom':'','weight_bottom':'','value_bottom':'','wage_bottom':''}
     int_searches_top = {'age_top':'','height_top':'','weight_top':'','weight_top':'','value_top':'','wage_top':''}
 
-    try:
-        asc = request.GET['asc']
-    except:
-        asc = 'asc'
-    orderByList = {'orderby1':request.GET['orderby1'],'orderby2':request.GET['orderby2'],'orderby3':request.GET['orderby3'],'orderby4':request.GET['orderby4'],'orderby5':request.GET['orderby5'],'asc':asc}
+    
 
+    orderByList = CreateOrderByList(request)
+    orderByString = CreateOrderByString(orderByList)
+    
     wherestring = CreateWhereString(request, text_searches, int_searches_bottom, int_searches_top)
-    query = "SELECT * FROM player WHERE 1=1 "+ wherestring +" ORDER BY weight desc limit 100"
+
+    query = "SELECT * FROM player WHERE 1=1 "+ wherestring + orderByString + " limit 500"
+    #print(query)
     players = Player.objects.raw(query)
     
     return(render(request,'home.html',{'players':players,'column_visibilities':column_visibilities,'text_searches':text_searches,'int_searches_bottom':int_searches_bottom,'int_searches_top':int_searches_top,'orderbylist':orderByList}))
+
+def clubwithchosen(request,chosenclub):
+    text_searches = {'clubname_txt':chosenclub}
+    query = "SELECT * FROM club WHERE 1=1 AND name LIKE '%%"+chosenclub+"%%'" # + wherestring + " limit 500"
+    clubs = Club.objects.raw(query)
+    return(render(request,'club.html',{'clubs':clubs,'text_searches':text_searches}))
+
+
+def club(request):
+    text_searches = {'name_txt':'','division_txt':'','based_txt':''}
+    wherestring = CreateWhereString(request,text_searches,{},{})
+    print(wherestring)
+    query = "SELECT * FROM club WHERE 1=1 " + wherestring #limit 500"
+    clubs = Club.objects.raw(query)
+    return(render(request,'club.html',{'clubs':clubs,'text_searches':text_searches}))
+
+
+
+
 
 
 
@@ -70,17 +92,19 @@ def ColumnVisibilitiesInit(column_visibilities):
 def CreateWhereString(request, text_searches, int_searches_bottom, int_searches_top):
     wherestring = ""
     for search in text_searches:
+        print(search)
         try:
             value = request.GET[search]
             if search in ['bestpos_txt','nation_txt']:
                 value = value.upper()
-            if search in ['name_txt','club_txt','preferredfoot_txt']:
+            if search in ['name_txt','club_txt','preferredfoot_txt','division_txt','based_txt']:
                 value = value[0].upper() + value [1::1]
             if len(value) > 0:
               wherestring += " AND " + search.split('_')[0] + " LIKE '%%" + value + "%%'"
             text_searches[search] = value
         except:
           continue
+    
 
     for search in int_searches_bottom:
         try:
@@ -102,7 +126,59 @@ def CreateWhereString(request, text_searches, int_searches_bottom, int_searches_
 
     return wherestring
 
+
 def CreateOrderByList(request):
-    orderByList=[]
+    orderByList = {'orderby1':'','orderby2':'','orderby3':'','orderby4':'','orderby5':'','asc':''}
+    
+    try:
+        orderByList['asc'] = request.GET['asc']
+    except:
+        orderByList['asc'] = 'asc'
+
+    try:
+        orderByList['orderby1'] = request.GET['orderby1']
+    except:
+        pass
+
+    try:
+        orderByList['orderby2'] = request.GET['orderby2']
+    except:
+        pass
+
+    try:
+        orderByList['orderby3'] = request.GET['orderby3']
+    except:
+        pass
+
+    try:
+        orderByList['orderby4'] = request.GET['orderby4']
+    except:
+        pass
+
+    try:
+        orderByList['orderby5'] = request.GET['orderby5']
+    except:
+        pass
 
     return orderByList
+
+
+def CreateOrderByString(orderByList):
+    orderByString = ''
+    for column in orderByList:
+        if orderByList[column] != '' and column != 'asc':
+            if orderByList[column] == 'Súly':
+                orderByString += ' Weight, '
+            elif orderByList[column] == 'Kor':
+                orderByString += ' Age, '
+            elif orderByList[column] == 'Magasság':
+                orderByString += ' Height, '
+            elif orderByList[column] == 'Érték':
+                orderByString += ' Value, '
+            elif orderByList[column] == 'Fizetés':
+                orderByString += ' Salary, '
+            else:
+                orderByString += orderByList[column] +', '
+    if orderByString != '':
+        orderByString = ' ORDER BY ' + orderByString[0:-2] + ' ' + orderByList['asc']
+    return orderByString
