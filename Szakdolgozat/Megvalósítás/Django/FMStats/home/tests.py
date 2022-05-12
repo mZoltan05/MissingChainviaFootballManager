969385891a9226e4c46a0e6f07b357f56021d4e7
Munkácsi Zoltán <munkacsizoltan05@gmail.com>
2022-05-12 15:49:03 +0200

    testcase1

--- Szakdolgozat/Megvalósítás/Django/FMStats/home/tests.py
+++ Szakdolgozat/Megvalósítás/Django/FMStats/home/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+#from Logic import Logic 
+import unittest
+from home.models import Player, Score
 
 # Create your tests here.
+def add(a,b):
+    return a+b
+
+class TestAdd(unittest.TestCase):
+     def test_add(self):
+          self.assertEqual(add(2, 4), 6)
+
+if __name__ == '__main__':
+    unittest.main()
